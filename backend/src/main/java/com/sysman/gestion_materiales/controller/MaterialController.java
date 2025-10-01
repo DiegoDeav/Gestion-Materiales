4308330b69b90d782172d6d7a2a5f4c7188f246e
Diego <deavila1430@gmail.com>
2025-09-30 22:11:17 -0500

    spring-boot -> folder backend

--- backend/src/main/java/com/sysman/gestion_materiales/controller/MaterialController.java
+++ backend/src/main/java/com/sysman/gestion_materiales/controller/MaterialController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/Materiales")
+@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Materiales", description = "Operacion de servicio CRUD para Materiales")
 public class MaterialController {
     private final MaterialService service;
